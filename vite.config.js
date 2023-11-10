63486591c29afe135417129f23209b031e111be1
EyK-26 <137394243+EyK-26@users.noreply.github.com>
2023-11-10 15:28:39 +0100

    setup config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,42 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig, loadEnv } from "vite";
+// import reactRefresh from "@vitejs/plugin-react-refresh";
+import laravel from "laravel-vite-plugin";
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-});
+export default ({ mode }) => {
+    const { APP_URL } = loadEnv(mode, process.cwd(), "");
+
+    return defineConfig({
+        plugins: [
+            laravel({
+               input: ["resources/css/app.scss", "resources/js/app.js"],
+
+                refresh: true,
+            }),
+            // reactRefresh(),
+            {
+                // fixes URLs that start with / in CSS files
+
+                // make sure to have vite.loadEnv imported and APP_URL defined
+
+                name: "css-static-url-fixer",
+
+                enforce: "post",
+
+                apply: "serve",
+
+                transform: (code, file) => {
+                    if (mode === "development" && file.match(/\.s?css($|\?)/)) {
+                        return {
+                            code: code.replaceAll(
+                                /url\(([\'\"]?)(\/[^\)\'\"]+)\1\)/g,
+                                `url($1${APP_URL}$2$1)`
+                            ),
+                        };
+                    }
+                },
+            },
+        ],
+    });
+};
+
+`
