26ad62c8284857ed9dbc81bfdf4d0a48b1115423
EyK-26 <137394243+EyK-26@users.noreply.github.com>
2023-11-13 16:39:19 +0100

    create api routes and controllers

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\ApplicationController;
+use App\Http\Controllers\Api\PositionController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,21 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/positions', [PositionController::class, 'index'])->name('positions.index');
+Route::post('/positions', [PositionController::class, 'store'])->name('positions.store');
+Route::get('/positions/{position}', [PositionController::class, 'show'])->whereNumber('position')->name('positions.show');
+Route::post('/positions/{position}/edit', [PositionController::class, 'update'])->whereNumber('position')->name('positions.update');
+Route::post('/positions/{position}/delete', [PositionController::class, 'destroy'])->whereNumber('position')->name('positions.destroy');
+
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::post('/users', [UserController::class, 'store'])->name('users.store');
+Route::get('/users/{user}', [UserController::class, 'show'])->whereNumber('user')->name('users.show');
+Route::post('/users/{user}/edit', [UserController::class, 'update'])->whereNumber('user')->name('users.update');
+Route::post('/users/{user}/delete', [UserController::class, 'destroy'])->whereNumber('user')->name('users.destroy');
+
+Route::get('/applications', [ApplicationController::class, 'index'])->name('applications.index');
+Route::post('/applications', [ApplicationController::class, 'store'])->name('applications.store');
+Route::get('/applications/{application}', [ApplicationController::class, 'show'])->whereNumber('application')->name('applications.show');
+Route::post('/applications/{application}/edit', [ApplicationController::class, 'update'])->whereNumber('application')->name('applications.update');
+Route::post('/applications/{application}/delete', [ApplicationController::class, 'destroy'])->whereNumber('application')->name('applications.destroy');
