74fda65ad2927cf77178cb66abdc6214e4fd4434
Ondrej.Kutina <onakutin@protonmail.com>
2023-11-14 17:31:04 +0100

    basic form, no functionality

--- resources/js/hiroapp/components/recruiter/Home.jsx
+++ resources/js/hiroapp/components/recruiter/Home.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
 	const [hirings, setHirings] = useState([]);
-	const [applicationsAmount, setApplicationsAmount] = useState(0);
 
 	const fetchHirings = async () => {
 		try {
@@ -22,17 +21,77 @@ const Home = () => {
 	return (
 		<>
 			<h1>Welcome Recruiter</h1>
-			<h2>Your hirings</h2>
+			<div style={{ display: "flex" }}>
+				<div style={{ display: "flex", flexDirection: "column" }}>
+					<h2>Your hirings</h2>
 
-			{hirings.map((hiring) => (
-				<Fragment key={hiring.id}>
-					<Link to={"/positions/" + hiring.id}>
-						<h4>{hiring.name}</h4>
-					</Link>
-					<p>{hiring.applications.length}</p>
-					<p>Due: {hiring.start_date ? hiring.start_date : "Not determined"}</p>
-				</Fragment>
-			))}
+					<div>
+						{hirings.map((hiring) => (
+							<Fragment key={hiring.id}>
+								<Link to={"/positions/" + hiring.id}>
+									<h4>{hiring.name}</h4>
+								</Link>
+								<p>{hiring.applications.length}</p>
+								<p>
+									Due:{" "}
+									{hiring.start_date ? hiring.start_date : "Not determined"}
+								</p>
+							</Fragment>
+						))}
+					</div>
+				</div>
+				<div>
+					<h2>Create new position</h2>
+					<form
+						action="/login"
+						method="post"
+						// onSubmit={handleSubmit}
+					>
+						<br />
+						Name:
+						<br />
+						<input
+							type="text"
+							name="name"
+							// value={values.attachment_text}
+							// onChange={handleChange}
+						/>
+						<br />
+						Description: <br />
+						<textarea
+							name="description"
+							// value={values.attachment_file}
+							// onChange={handleChange}
+						/>
+						<br />
+						Start date: <br />
+						<input
+							type="date"
+							name="start_date"
+							// value={values.attachment_file}
+							// onChange={handleChange}
+						/>
+						<br />
+						End date: <br />
+						<input
+							type="date"
+							name="end_date"
+							// value={values.attachment_file}
+							// onChange={handleChange}
+						/>
+						<br />
+						Pay grade: <br />
+						<input
+							type="number"
+							name="pay_grade"
+							// value={values.attachment_file}
+							// onChange={handleChange}
+						/>
+						<br />
+						<button>Create</button>
+					</form>
+				</div>
+			</div>
 		</>
 	);
 };
