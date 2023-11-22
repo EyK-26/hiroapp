acc87b0f5c114b0980081e219889fb37e910069c
Pe3k <jakub53k@gmail.com>
2023-11-22 11:57:17 +0100

    favicon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="#">
+    <link rel="icon" type="image/x-icon" href="favicon.png">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @vite('resources/css/app.scss')
     <title>Document</title>
