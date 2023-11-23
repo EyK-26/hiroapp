dc63be65d27c7fd1d7614b4d3e997e411097db97
Pe3k <jakub53k@gmail.com>
2023-11-23 09:57:31 +0100

    added footer and bug fix

--- resources/js/hiroapp/components/Footer.jsx
+++ resources/js/hiroapp/components/Footer.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Footer = () => {
-    return <div className="footer">Footer</div>;
+    return (
+        <div className="footer">&copy; HiroApp {new Date().getFullYear()}</div>
+    );
 };
 
 export default Footer;
