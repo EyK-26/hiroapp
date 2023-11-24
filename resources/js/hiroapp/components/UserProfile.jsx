579b91cf92b34e6736970f2216aff79d5becb80d
Pe3k <jakub53k@gmail.com>
2023-11-24 13:30:29 +0100

    nav for mobile

--- resources/js/hiroapp/components/UserProfile.jsx
+++ resources/js/hiroapp/components/UserProfile.jsx
@@ -2,12 +2,16 @@ import React, { useContext } from "react";
 import Logout from "./auth/Logout";
 import Context from "../context/Context";
 import Inbox from "./Inbox";
+import Navigation from "./Navigation";
 
 const UserProfile = () => {
     const { state } = useContext(Context);
 
     return (
         <div className="UserProfile">
+            <div className="NavigationMobile">
+                <Navigation />
+            </div>
             <span className="username">
                 {state.user?.first_name} {state.user?.last_name}
             </span>
